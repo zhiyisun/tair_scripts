f43fe53b8d0770a2e547fb998fb116d32ed6f107
Zhiyi Sun <zhiyisun@msn.com>
2016-10-13 05:56:38 +0800

    Add default setting for myclient.sh
    Signed-off-by: Zhiyi Sun <zhiyisun@msn.com>

--- myclient.sh
+++ myclient.sh
@@ -19,10 +19,18 @@
 
 set -o nounset                              # Treat unset variables as an error
 
-SERVER=12.0.0.100
+if [ -z ${SERVER+x} ]
+then
+	SERVER=13.0.0.3
+fi
+
+if [ -z ${TAIR_DIR+x} ]
+then
+	TAIR_DIR=/home/zsun/workspace/code/tair
+fi
+
 PORT=5198
 GROUP=group_1
-TAIR_DIR=/home/zsun/workspace/code/tair
 TAIR_CS=${TAIR_DIR}/tair_bin_cs
 cd ${TAIR_CS}/sbin
 ./tairclient -c ${SERVER}:${PORT} -g ${GROUP}
